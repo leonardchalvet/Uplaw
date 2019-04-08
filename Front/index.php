3d6dbb606e067217c8ae0d86973df0c13e00de5f
Léonard <leonard@agence-me.com>
2019-04-08 15:05:43 +0200

    Add Front Folder

---
+++ Front/index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+
+		<title>Title</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/home.css">
+
+		<script type="text/javascript" src="script/minify/jquery-3.3.1-min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php include('common-header.php') ?>
+
+		<main>
+			
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/index-min.js"></script>
+	</body>
+</html>
