be3d4b5b194ab6fcf035d46e81e2b12fb21db984
Léonard <leonard@agence-me.com>
2019-04-24 15:35:12 +0200

    Anim cover features

--- Front/script/js/features.js
+++ Front/script/js/features.js
@@ -1,5 +1,33 @@
 $(window).on('load', function() {
 
+	function animSectionHome(){
+
+		var wrapLineTransitionDuration = $('.wrapLine span').css('transition-duration');
+		var wrapLineTransitionDelay = $('.wrapLine span').css('transition-delay');
+
+		var countLineH1 = $('#section-cover .container-text h1.wrapLine span').length;
+
+		var delayH1 = (wrapLineTransitionDelay.replace('s', '') * countLineH1) * 1000;
+
+
+		$('#section-cover').addClass('anim');
+		setTimeout(function() {
+			$('#section-cover .container-text h1.wrapLine').addClass('anim');
+
+			setTimeout(function() {
+
+				
+
+				
+
+			}, delayH1);
+
+		}, 50);
+
+	}
+	animSectionHome();
+
+
 	if (window.matchMedia("(min-width: 700px)").matches) {
 
 		$('#section-features .obj-1').attr('data-parallax', '{"y": 60, "smoothness": 50}');
