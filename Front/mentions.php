eb537fbf3e151e8e2ce82218bb55c02cec4669e5
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-04-30 11:20:12 +0200

    Start page mentions legal

---
+++ Front/mentions.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+
+		<title>Mentions légales</title>
+
+		<meta name="description" content="" />
+
+		<meta http-equiv="content-type" content="text/html; charset=utf8" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="style/css/mentions.css">
+
+		<script type="text/javascript" src="script/minify/jQuery.3.3.1-min.js"></script>
+
+		<script type="text/javascript" src="script/minify/jquery.parallax-scroll-min.js"></script>
+
+	</head>
+	
+	<body>
+
+		<?php 
+		$type = 'apropos';
+		include('common-header.php') 
+		?>
+
+		<main>
+
+			<section id="section-mentions">
+				<div class="wrapper">
+					
+				</div>
+			</section>
+			
+		</main>
+
+		<?php include('common-footer.php') ?>
+
+		<script type="text/javascript" src="script/minify/common-min.js"></script>
+
+		<script type="text/javascript" src="script/minify/mentions-min.js"></script>
+	</body>
+</html>
