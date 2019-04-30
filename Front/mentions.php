d66f574a3a462bb74131e1caee44d8f1f0f8e8f8
Alexis Prevost-Maynen <killer76520@gmail.com>
2019-04-30 12:00:56 +0200

    Structure page mentions legale

--- Front/mentions.php
+++ Front/mentions.php
@@ -27,8 +27,65 @@
 		<main>
 
 			<section id="section-mentions">
+				<img class="obj-1" src="img/mentions/mentions-obj-1.svg">
+				<img class="obj-2" src="img/mentions/mentions-obj-2.svg">
 				<div class="wrapper">
 					
+					<div class="container-text">
+						<h1>Mentions Légales</h1>
+						<div class="container-el">
+							<div class="el">
+								<h2>Lorem ipsum</h2>
+								<p>
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+								</p>
+							</div>
+							<div class="sep"></div>
+							<div class="el">
+								<h2>Lorem ipsum</h2>
+								<p>
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+								</p>
+							</div>
+							<div class="sep"></div>
+							<div class="el">
+								<h2>Lorem ipsum</h2>
+								<p>
+									Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<aside>
+						<h2>Informations</h2>
+						<p>
+							UPLAW, société d'exercice libéral à responsiabilité limitée au capital de 1.000 euros, dont le siège social est situé au 151, boulevard Haussmann - 75008 Paris.
+						</p>
+						<div class="container-links">
+							<a href="">
+								<img src="img/mentions/mentions-contact.svg">
+								<span>contact@up.law</span>
+							</a>
+							<a href="">
+								<img src="img/mentions/mentions-phone.svg">
+								<span>contact@up.law</span>
+							</a>
+						</div>
+						<p>
+							Ce site est hébergé auprès de la société OVH :
+							SAS au capital de 10 069 020 €
+							RCS Lille Métropole 424 761 419 00045
+							Code APE 2620Z
+							N° TVA : FR 22 424 761 419
+
+							Siège social : 
+							2 rue Kellermann – 59100 Roubaix – France
+
+							UPLAW est un cabinet d’avocats inscrit auprès de l’Ordre des Avocats du Barreau de Paris sous le numéro de toque B863.
+						</p>
+					</aside>
+
 				</div>
 			</section>
 			
